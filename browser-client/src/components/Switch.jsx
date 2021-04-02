b9ae4d14405bcbd26f351c3990cf5c368dcf73a0
dabakos <dabasi.akos@gmail.com>
2021-04-02 16:21:48 +0200

    Webpage[Switch] - redefining the interface

--- browser-client/src/components/Switch.jsx
+++ browser-client/src/components/Switch.jsx
@@ -3,13 +3,13 @@ import "./Switch.css";
 
 class Switch extends Component {
   render() {
-    const { type, isOn } = this.props.device;
+    const { className, device, onClickHandler } = this.props;
     return (
       <input
-        className={"switch " + type}
+        className={"switch " + (className ? className : "")}
         type="checkbox"
-        checked={isOn}
-        onChange={() => this.props.onClickHandler(this.props.device)}
+        checked={device.isOn}
+        onChange={() => onClickHandler(this.props.device)}
       />
     );
   }
