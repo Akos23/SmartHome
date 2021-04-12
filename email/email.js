5745f8d4022841a03e2dadd4ea672cefc8ff78e7
dabakos <dabasi.akos@gmail.com>
2021-04-12 17:32:49 +0200

    Email - refactoring, changing topic name, expecting JSON format

--- email/email.js
+++ email/email.js
@@ -4,7 +4,7 @@ const mqtt = require("mqtt");
 const mqtt_server = "mqtt://192.168.1.19:1883";
 const mqtt_username = "email service";
 const mqtt_password = "19961224";
-const mqtt_topic = "notifications";
+const mqtt_topics = ["update/alarm"];
 
 const email_sender = "bmesmarthome.server@gmail.com";
 const email_password = "rFs0945kG";
@@ -26,16 +26,20 @@ const client = mqtt.connect(mqtt_server, {
 });
 
 client.on("connect", () => {
-  client.subscribe(mqtt_topic);
+  for (let i = 0; i < mqtt_topics.length; i++) {
+    client.subscribe(mqtt_topics[i]);
+  }
 });
 
 client.on("error", (err) => console.log(err.toString()));
 
 client.on("message", function (topic, message) {
+  const data = JSON.parse(message.toString());
+
   //add time-stamp
   const now = new Date();
 
-  if (message.toString() === "intrusion") {
+  if (topic.toString() === "update/alarm" && data.isOn) {
     //Sending an email alert
     const mailOptions = {
       from: email_sender,
